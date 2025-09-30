7e3c9a7b2f2f837e1c3c61759e72ed1c12afd17b
Guntur Lailam Yuro <gunturlailamyuro8@gmail.com>
2025-10-01 06:20:18 +0700

    add view formitem

---
+++ app/Views/formitem.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Item</title>
+</head>
+
+<body>
+    <center>
+        <form method="post" name="inputbrg" action="<?php base_url('Produk/simpan') ?>">
+            <table>
+                <tr>
+                    <td>Kode Barang</td>
+                    <td>
+                        <select name="kodebrg" onchange="Tampil()">
+                            <option>-PILIH-</option>
+                        </select>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+
+</html>
